5da4e6ac2d64f4a138b28e23e665e80122184a19
Abdulmalik Olaiya <olaiya.abdulmalik@gmail.com>
2025-11-09 19:48:35 +0100

    fixed directroy diffeences

--- my-company/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  const style = { textAlign: "center", padding: "2rem" };
-  return (
-    <div style={style}>
-      <h1>Welcome to My Company</h1>
-      <p>We deliver innovative solutions to help your business grow.</p>
-    </div>
-  );
-}
